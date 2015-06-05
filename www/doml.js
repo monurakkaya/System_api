a891826e310bbbd9fceecb2fb69f2936a24e97f7
Webcraft <webcraft007@gmail.com>
2015-06-05 18:59:05 +0100

    .

---
+++ www/doml.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+    getDMD:function(args) {
+        cordova.exec(
+            (!args.success) ? null : args.success, 
+            function(error){alert('DOMLauncher API getDMD Error:'+error)}, 
+            "doml", 
+            'getDMD',
+            []
+        );
+    },
+    
+    setDMD:function(args) {
+        cordova.exec(
+            (!args.success) ? null : args.success, 
+            function(error){alert('DOMLauncher API setDMD Error:'+error)}, 
+            "doml", 
+            'setDMD',
+            [{name:args.name}]
+        );                
+    }   
+};
+
+document.addEventListener("deviceready", cordovaBack, false);
+
+function cordovaBack(){
+    document.addEventListener("backbutton", backButton, false);
+}
+
+function backButton(){}
